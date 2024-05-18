9deef35dc396a3e6ecbd4558fc3c4f22f1be22b4
Mahdad Baghani <mahdad.baghani@outlook.com>
2024-05-18 03:54:10 +0200

    add gas report

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  gasReporter: {
+    currency: 'USD',
+    // L1: "ethereum",
+    L2:"arbitrum",
+    "L2Etherscan":"YV23NEQ48TRV4TN7ZY4YEFS99ANWWWN3UG",
+    coinmarketcap: "e63d6069-011a-4e3a-a165-1ca4a622a4f8",
+  }
 };
 
 export default config;
