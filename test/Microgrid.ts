e0cb7d2674a06bbf98b8568a064e9bc43dd89117
Mahdad Baghani <mahdad.baghani@outlook.com>
2024-06-18 23:35:54 +0200

    update test suite

--- test/Microgrid.ts
+++ test/Microgrid.ts
@@ -12,17 +12,20 @@ describe("MicrogridExchange", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployMarket() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner] = await ethers.getSigners();
     console.time("deploy market grid");
     const gridFactory = await ethers.getContractFactory("MicrogridMarket");
     const microgrid = await gridFactory.deploy(owner.address);
-
-    const smartHomeFactory = await ethers.getContractFactory("SmartHomeFactory");
-    const smartHome = await smartHomeFactory.deploy();
-
     console.timeEnd("deploy market grid");
 
-    return { microgrid, owner, otherAccount, smartHome };
+    return { microgrid, owner };
+  }
+
+  async function deploySmartHomes() {
+    const smartHomeFactory = await ethers.getContractFactory("SmartHomeFactory");
+    const factory = await smartHomeFactory.deploy();
+
+    return { factory }
   }
 
   describe("Deployment", function () {
@@ -31,102 +34,67 @@ describe("MicrogridExchange", function () {
 
       expect(await microgrid.owner()).to.equal(owner.address);
     });
-
-
-    // it("Should fail if the unlockTime is not in the future", async function () {
-    //   // We don't use the fixture here because we want a different deployment
-    //   const latestTime = await time.latest();
-    //   const Lock = await ethers.getContractFactory("Lock");
-    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //     "Unlock time should be in the future"
-    //   );
-    // });
   });
 
-  describe("smartHome tests", function (){
-    
+  describe("smartHome tests", function () {
+
     it("Should correctly create the smarthome contract on blockchain", async function () {
-        const { smartHome } = await loadFixture(deployMarket);
-        const tx = await smartHome.createHousehold(1000)
-        await tx.wait();
-        
-        const households = await smartHome.getDeployedHouseholds();
-        // first way of getting event data
-        const filter = smartHome.filters.SmartHomeCreated;
-        const events = await smartHome.queryFilter(filter, -1);
-        const event = events[0];
-        await expect(event.args[0].valueOf()).to.equal(households[0])
+      const [_, ...otherAccounts] = await ethers.getSigners();
+      const { factory } = await loadFixture(deploySmartHomes);
+      const tx = await factory.connect(otherAccounts[0]).createHousehold(1000)
+      await tx.wait();
 
-        // second way of getting event data
-        await expect(tx).to.emit(smartHome, "SmartHomeCreated").withArgs(households[0]);
+      const households = await factory.getDeployedHouseholds();
+      // first way of getting event data
+      const filter = factory.filters.SmartHomeCreated;
+      const events = await factory.queryFilter(filter, -1);
+      const event = events[0];
+      await expect(event.args[0].valueOf()).to.equal(households[0])
 
+      // second way of getting event data
+      await expect(tx).to.emit(factory, "SmartHomeCreated").withArgs(households[0]);
+
+      const sh = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+      await expect(await sh.owner()).to.equal(otherAccounts[0].address)
+    });
+
+    it("Should correctly match the buy and sell orders", async function () {
+      const [_, ...otherAccounts] = await ethers.getSigners();
+
+      const { microgrid } = await loadFixture(deployMarket);
+      const { factory } = await loadFixture(deploySmartHomes);
+
+      const tx1 = await factory.connect(otherAccounts[0]).createHousehold(1000);
+      tx1.wait();
+      
+      const tx2 = await factory.connect(otherAccounts[1]).createHousehold(1000);
+      tx2.wait();
+
+      const filter = factory.filters.SmartHomeCreated;
+      let events = await factory.queryFilter(filter, -1);
+      let event = events[0];
+      const sh1 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+      events = await factory.queryFilter(filter, -1);
+      event = events[1];
+      const sh2 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+      
+      await expect(await sh1.owner()).to.equal(otherAccounts[0].address)
+      await expect(await sh2.owner()).to.equal(otherAccounts[1].address)
+
+
+
+      await sh1.setExchange(microgrid.getAddress());
+      await sh2.setExchange(microgrid.getAddress());
+
+      await otherAccounts[1].sendTransaction({
+        to: sh2.getAddress(),
+        value: ethers.parseEther("0.5"),
       });
-    
+      
+      await sh1.connect(otherAccounts[0]).submitAsk(10, 1, Date.now());
+      await sh2.connect(otherAccounts[1]).submitBid(11, 1, Date.now())
+
+      await expect(await ethers.provider.getBalance(sh1.getAddress())).to.equal(11)
+    });
   });
-
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
 });
