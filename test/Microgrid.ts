bd127f05434b415c4ead594648fc76985eca563d
Mahdad Baghani <mahdad.baghani@outlook.com>
2024-07-09 23:39:02 +0200

    add new tests

--- test/Microgrid.ts
+++ test/Microgrid.ts
@@ -21,15 +21,57 @@ describe("MicrogridExchange", function () {
     return { microgrid, owner };
   }
 
-  async function deploySmartHomes() {
+  async function deploySmartHomeFactory() {
     const smartHomeFactory = await ethers.getContractFactory("SmartHomeFactory");
     const factory = await smartHomeFactory.deploy();
-
-    return { factory }
+    return factory;
   }
 
-  describe("Deployment", function () {
-    it("Should set the right owner", async function () {
+  async function deploy1SmartHome() {
+    const [_, ...otherAccounts] = await ethers.getSigners();
+
+    const factory = await deploySmartHomeFactory();
+
+    const tx = await factory.connect(otherAccounts[0]).createHousehold(1000);
+    tx.wait();
+    const households = await factory.getDeployedHouseholds();
+
+    // first way of getting event data
+    const filter = factory.filters.SmartHomeCreated;
+    let events = await factory.queryFilter(filter, -1);
+    let event = events[0];
+    await expect(event.args[0].valueOf()).to.equal(households[0])
+    // second way of getting event data
+    await expect(tx).to.emit(factory, "SmartHomeCreated").withArgs(households[0]);
+    
+    const sh1 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+    return { factory, SmartHome1: sh1, account1: otherAccounts[0] }
+  }
+  
+  async function deploy2SmartHomes() {
+    const [_, ...otherAccounts] = await ethers.getSigners();
+
+    const factory = await deploySmartHomeFactory();
+
+    const tx1 = await factory.connect(otherAccounts[0]).createHousehold(1000);
+    tx1.wait();
+    
+    const tx2 = await factory.connect(otherAccounts[1]).createHousehold(1000);
+    tx2.wait();
+
+    const filter = factory.filters.SmartHomeCreated;
+    let events = await factory.queryFilter(filter, -1);
+    let event = events[0];
+    const sh1 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+    events = await factory.queryFilter(filter, -1);
+    event = events[1];
+    const sh2 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
+
+    return { factory, SmartHome1: sh1, SmartHome2: sh2, account1: otherAccounts[0], account2: otherAccounts[1] }
+  }
+
+  describe("Microgrid Deployment", function () {
+    it("Should set the right owner for MicroGrid market", async function () {
       const { microgrid, owner } = await loadFixture(deployMarket);
 
       expect(await microgrid.owner()).to.equal(owner.address);
@@ -40,61 +82,98 @@ describe("MicrogridExchange", function () {
 
     it("Should correctly create the smarthome contract on blockchain", async function () {
       const [_, ...otherAccounts] = await ethers.getSigners();
-      const { factory } = await loadFixture(deploySmartHomes);
-      const tx = await factory.connect(otherAccounts[0]).createHousehold(1000)
-      await tx.wait();
-
-      const households = await factory.getDeployedHouseholds();
-      // first way of getting event data
-      const filter = factory.filters.SmartHomeCreated;
-      const events = await factory.queryFilter(filter, -1);
-      const event = events[0];
-      await expect(event.args[0].valueOf()).to.equal(households[0])
-
-      // second way of getting event data
-      await expect(tx).to.emit(factory, "SmartHomeCreated").withArgs(households[0]);
-
-      const sh = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
-      await expect(await sh.owner()).to.equal(otherAccounts[0].address)
+      const { SmartHome1, account1 } = await loadFixture(deploy1SmartHome);
+      expect(await SmartHome1.owner()).to.equal(otherAccounts[0].address);
+      expect(await SmartHome1.owner()).to.equal(account1.address);
     });
 
     it("Should correctly match the buy and sell orders", async function () {
-      const [_, ...otherAccounts] = await ethers.getSigners();
 
       const { microgrid } = await loadFixture(deployMarket);
-      const { factory } = await loadFixture(deploySmartHomes);
-
-      const tx1 = await factory.connect(otherAccounts[0]).createHousehold(1000);
-      tx1.wait();
+      const { SmartHome1, SmartHome2, account1, account2 } = await loadFixture(deploy2SmartHomes);
       
-      const tx2 = await factory.connect(otherAccounts[1]).createHousehold(1000);
-      tx2.wait();
+      expect(await SmartHome1.owner()).to.equal(account1.address);
+      expect(await SmartHome2.owner()).to.equal(account2.address);
 
-      const filter = factory.filters.SmartHomeCreated;
-      let events = await factory.queryFilter(filter, -1);
-      let event = events[0];
-      const sh1 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
-      events = await factory.queryFilter(filter, -1);
-      event = events[1];
-      const sh2 = await ethers.getContractAt("SmartHome", event.args[0].valueOf())
-      
-      await expect(await sh1.owner()).to.equal(otherAccounts[0].address)
-      await expect(await sh2.owner()).to.equal(otherAccounts[1].address)
+      await SmartHome1.setExchange(microgrid.getAddress());
+      await SmartHome2.setExchange(microgrid.getAddress());
 
-
-
-      await sh1.setExchange(microgrid.getAddress());
-      await sh2.setExchange(microgrid.getAddress());
-
-      await otherAccounts[1].sendTransaction({
-        to: sh2.getAddress(),
+      await account2.sendTransaction({
+        to: SmartHome2.getAddress(),
         value: ethers.parseEther("0.5"),
       });
       
-      await sh1.connect(otherAccounts[0]).submitAsk(10, 1, Date.now());
-      await sh2.connect(otherAccounts[1]).submitBid(11, 1, Date.now())
+      await SmartHome1.connect(account1).submitAsk(10, 1, Date.now());
+      await SmartHome2.connect(account2).submitBid(11, 1, Date.now())
 
-      await expect(await ethers.provider.getBalance(sh1.getAddress())).to.equal(11)
+      expect(await ethers.provider.getBalance(SmartHome1.getAddress())).to.equal(11)
     });
+
+    it("Should prevent sending trust score before interaction", async () => {
+      const { microgrid } = await loadFixture(deployMarket);
+      const { SmartHome1, SmartHome2, account1, account2 } = await loadFixture(deploy2SmartHomes);
+      
+      expect(await SmartHome1.owner()).to.equal(account1.address);
+      expect(await SmartHome2.owner()).to.equal(account2.address);
+
+      await SmartHome1.setExchange(microgrid.getAddress());
+      await SmartHome2.setExchange(microgrid.getAddress());
+
+      await expect(microgrid.connect(account1).rateInteraction(account2, 5)).to.be.reverted;
+      await expect(microgrid.connect(account2).rateInteraction(account1, 5)).to.be.reverted;
+    })
+
+    it("Should should submit trust score after interaction", async () => {
+      const { microgrid } = await loadFixture(deployMarket);
+      const { SmartHome1, SmartHome2, account1, account2 } = await loadFixture(deploy2SmartHomes);
+      
+      expect(await SmartHome1.owner()).to.equal(account1.address);
+      expect(await SmartHome2.owner()).to.equal(account2.address);
+
+      await SmartHome1.setExchange(microgrid.getAddress());
+      await SmartHome2.setExchange(microgrid.getAddress());
+
+      await account2.sendTransaction({
+        to: SmartHome2.getAddress(),
+        value: ethers.parseEther("0.5"),
+      });
+      
+      await SmartHome1.connect(account1).submitAsk(10, 1, Date.now());
+      await SmartHome2.connect(account2).submitBid(11, 1, Date.now());
+
+      expect(await ethers.provider.getBalance(SmartHome1.getAddress())).to.equal(11)
+
+      await expect(microgrid.connect(account1).rateInteraction(account2, 5)).to.not.be.reverted;
+      await expect(microgrid.connect(account2).rateInteraction(account1, 5)).to.not.be.reverted;
+    })
+
+    it("Should prevent double-spending on trust score", async () => {
+      const { microgrid } = await loadFixture(deployMarket);
+      const { SmartHome1, SmartHome2, account1, account2 } = await loadFixture(deploy2SmartHomes);
+      
+      expect(await SmartHome1.owner()).to.equal(account1.address);
+      expect(await SmartHome2.owner()).to.equal(account2.address);
+
+      await SmartHome1.setExchange(microgrid.getAddress());
+      await SmartHome2.setExchange(microgrid.getAddress());
+
+      await account2.sendTransaction({
+        to: SmartHome2.getAddress(),
+        value: ethers.parseEther("0.5"),
+      });
+      
+      await SmartHome1.connect(account1).submitAsk(10, 1, Date.now());
+      await SmartHome2.connect(account2).submitBid(11, 1, Date.now());
+
+      expect(await ethers.provider.getBalance(SmartHome1.getAddress())).to.equal(11)
+
+      await expect(microgrid.connect(account1).rateInteraction(account2, 5)).to.not.be.reverted;
+      await expect(microgrid.connect(account2).rateInteraction(account1, 5)).to.not.be.reverted;
+      
+      await expect(microgrid.connect(account1).rateInteraction(account2, 5)).to.be.reverted;
+
+    })
   });
 });
+
+
